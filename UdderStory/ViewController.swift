71d556b15e1ad067a74dd0c56656369781343d6d
irysh-k <irina.kravchenko772@gmail.com>
2024-10-29 21:09:05 +0100

    Initial Commit

---
+++ UdderStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdderStory
+//
+//  Created by user265012 on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UdderStoryTests/UdderStoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderStoryTests.swift
+//  UdderStoryTests
+//
+//  Created by user265012 on 10/29/24.
+//
+
+import Testing
+@testable import UdderStory
+
+struct UdderStoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
